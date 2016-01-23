d75c343b047a9283d9bb618a09d0672e8db01941
Lucas <lucas@agtlucas.com>
2016-01-22 22:23:50 -0200

    Updates

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,32 @@ import React, { Component, PropTypes } from 'react'
 import { render } from 'react-dom'
 
 import styles from '../styles/main.styl'
+styles
 
 class App extends Component {
 
+  constructor (props) {
+    super(props)
+    this.state = {
+      name: ''
+    }
+  }
+
+  onChangeName (e) {
+    this.setState({ name: e.target.value })
+  }
+
   render () {
     return (
-      <div>
-        <p>Ok!</p>
-      </div>
+      <section>
+        <div className='wrapper'>
+          <p>Hey!</p>
+          <div>
+            <input onChange={this.onChangeName.bind(this)} ref='name' placeholder='name' />
+            <p>{this.state.name}</p>
+          </div>
+        </div>
+      </section>
     )
   }
 
